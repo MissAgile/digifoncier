50bb75e842635d81d04b112ec4d4e8ea7aa86df0
mbaye <rokhymbaye013@gmail.com>
2025-02-22 18:02:11 +0000

    Message de commit pour enregistrer mes modifications

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'contacts',
+  selector: 'app-contacts',
   standalone: true,
   imports: [],
-  templateUrl: 'contacts.component.html',
+  templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {

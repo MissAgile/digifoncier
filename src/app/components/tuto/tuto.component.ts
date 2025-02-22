028a448af5dc733574d2f3112ab9ef5950c58de2
MissAgile <khadijacisse023@gmail.com>
2025-02-22 14:34:13 +0000

    first commitFHF

---
+++ src/app/components/tuto/tuto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tuto',
+  imports: [],
+  templateUrl: './tuto.component.html',
+  styleUrl: './tuto.component.css'
+})
+export class TutoComponent {
+
+}
